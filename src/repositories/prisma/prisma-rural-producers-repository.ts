04dbe7a45b57fb828795aa423ca6bc3053c10934
William Furtunato da Silva <williamfurtunato@hotmail.com>
2024-02-12 16:18:12 -0300

    feat: prisma rural producer repository

---
+++ src/repositories/prisma/prisma-rural-producers-repository.ts
@@ -0,0 +1,48 @@
+import { Prisma } from '@prisma/client'
+import { RuralProducersRepository } from '../rural-producers-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaRuralProducersRepository
+  implements RuralProducersRepository
+{
+  async create(data: Prisma.RuralProducerCreateInput) {
+    const producer = await prisma.ruralProducer.create({ data })
+
+    return producer
+  }
+
+  async delete(id: string) {
+    await prisma.ruralProducer.delete({
+      where: {
+        id,
+      },
+    })
+
+    return id
+  }
+
+  async update(id: string, data: Prisma.RuralProducerUpdateInput) {
+    const ruralProducer = await prisma.ruralProducer.update({
+      where: { id },
+      data,
+    })
+
+    return ruralProducer
+  }
+
+  async findByDocument(document: string) {
+    const ruralProducer = await prisma.ruralProducer.findUnique({
+      where: { document },
+    })
+
+    return ruralProducer
+  }
+
+  async findById(id: string) {
+    const ruralProducer = await prisma.ruralProducer.findUnique({
+      where: { id },
+    })
+
+    return ruralProducer
+  }
+}
