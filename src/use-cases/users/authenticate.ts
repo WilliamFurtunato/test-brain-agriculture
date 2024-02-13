b2e8e9a28a9a0bed2d2f0462a2097415d53000cc
William Furtunato da Silva <williamfurtunato@hotmail.com>
2024-02-13 12:07:13 -0300

    feat: authenticate use case

---
+++ src/use-cases/users/authenticate.ts
@@ -0,0 +1,36 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { compare } from 'bcryptjs'
+import { User } from '@prisma/client'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+
+interface AuthenticateUseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseRequest): Promise<AuthenticateUseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
+}
